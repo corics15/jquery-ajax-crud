3e92d6adb1ed3a29c6423dd6b58bc688861cc212
corics15 <corics15@gmail.com>
2016-12-05 13:04:03 +0800

    Table with sample data

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -3,7 +3,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Main extends CI_Controller {
 
+	public function __construct() {
+		parent:: __construct();
+		$this->load->model('User_model');
+	}
+
 	public function index()	{
-		$this->load->view('main');
+		$data['user_info'] = $this->User_model->getAllUsers();
+		$this->load->view('main', $data);
 	}
 }
